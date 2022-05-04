e5bd2756d250b7bae77467d8432a24eec421a45f
Hello <65427576+603550@users.noreply.github.com>
2022-05-04 00:47:28 -0400

    Update stripe.js

--- stripe.js
+++ stripe.js
@@ -46,6 +46,37 @@ const appearance = {
   }
 };
 
+        
+        
+        const appearance = {
+    // If you are planning to extensively customize rules, use the "none"
+    // theme. This theme provides a minimal number of rules by default to avoid
+    // interfering with your custom rule definitions.
+     labels: 'floating',
+     theme: 'none',
+
+    rules: {
+      '.Tab': {
+        border: '1px solid #E0E6EB',
+        boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02)',
+      },
+
+      '.Tab:hover': {
+        color: 'var(--colorText)',
+      },
+
+      '.Tab--selected': {
+        borderColor: '#E0E6EB',
+        boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02), 0 0 0 2px var(--colorPrimary)',
+      },
+
+      '.Input--invalid': {
+        boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 0 0 2px var(--colorDanger)',
+      },
+
+      // See all supported class names and selector syntax at https://stripe.com/docs/stripe-js/appearance-api#variables
+    }
+  };
           
                 var elements = stripe.elements({
                 clientSecret: 'pi_3KvGkfKYRnOmsRcr14sCKDGb_secret_3t0G0i1ENsSqzMgZGFC8Zxs1I', appearance
